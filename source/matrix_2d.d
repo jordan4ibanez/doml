505d78951f0fc8bf8a7f0c3ad111428724e850c8
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-16 23:49:55 -0400

    Delete tostring

--- source/matrix_2d.d
+++ source/matrix_2d.d
@@ -421,45 +421,7 @@ struct Matrix2d {
         return dest;
     }
 
-    /**
-     * Return a string representation of this matrix.
-     * <p>
-     * This method creates a new {@link DecimalFormat} on every invocation with the format string "<code>0.000E0;-</code>".
-     *
-     * @return the string representation
-     */
-    public String toString() {
-        String str = toString(Options.NUMBER_FORMAT);
-        StringBuffer res = new StringBuffer();
-        int eIndex = Integer.MIN_VALUE;
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (c == 'E') {
-                eIndex = i;
-            } else if (c == ' ' && eIndex == i - 1) {
-                // workaround Java 1.4 DecimalFormat bug
-                res.append('+');
-                continue;
-            } else if (Character.isDigit(c) && eIndex == i - 1) {
-                res.append('+');
-            }
-            res.append(c);
-        }
-        return res.toString();
-    }
-
-    /**
-     * Return a string representation of this matrix by formatting the matrix elements with the given {@link NumberFormat}.
-     *
-     * @param formatter
-     *          the {@link NumberFormat} used to format the matrix values with
-     * @return the string representation
-     */
-    public String toString(NumberFormat formatter) {
-        return Runtime.format(m00, formatter) + " " + Runtime.format(m10, formatter) + "\n"
-             + Runtime.format(m01, formatter) + " " + Runtime.format(m11, formatter) + "\n";
-    }
-
+   
     /**
      * Get the current values of <code>this</code> matrix and store them into
      * <code>dest</code>.
