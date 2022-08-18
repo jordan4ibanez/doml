c1d41f676b6e8b612ef775c42901aaf304931b50
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-18 05:10:06 -0400

    Update app.d

--- source/app.d
+++ source/app.d
@@ -6,11 +6,15 @@ import tests.dunit_tests;
 
 import tests.math_test;
 import tests.axis_angle_test;
+// import tests.frustum_intersect;
+import tests.matrix_4d_test;
 
 
 void main()
 {
-    setTestVerbose(false);
+    setTestVerbose(true);
 	testMath();
     testAxisAngle();
+    // testFrustumIntersect();
+    testMatrix4D();
 }
