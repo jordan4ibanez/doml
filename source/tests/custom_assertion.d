8dacb1fba791a9af9b50a21ee42f71956f25cf3b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-18 02:22:42 -0400

    More DUNIT tests

--- source/tests/custom_assertion.d
+++ source/tests/custom_assertion.d
@@ -2,10 +2,18 @@ module tests.custom_assertion;
 
 // This was written SPECIFICALLY to get the precision capabilities of JUNIT
 
+// I'll call it, DUNIT-mini
 
 void assertEquals(double a, double b, int precision) {
     int c = cast(int)(a * precision);
     int d = cast(int)(b * precision);
-
     assert(c == d);
+}
+
+void assertFalse(bool value) {
+    assert(value == false);
+}
+
+void assertTrue(bool value) {
+    assert(value == true);
 }
