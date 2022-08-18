aa780944ad232e6beaf06bc7dd195295c90f99ae
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-08-18 02:31:50 -0400

    Allow verbose tests

--- source/tests/custom_assertion.d
+++ source/tests/custom_assertion.d
@@ -1,16 +1,30 @@
 module tests.custom_assertion;
 
+import std.stdio;
+
 // This was written SPECIFICALLY to get the precision capabilities of JUNIT
 
 // I'll call it, DUNIT-mini
 
+private bool verbose = false;
+
+void setTestVerbose(bool newValue) {
+    verbose = newValue;
+}
+
 void assertEquals(double a, double b, int precision) {
     int c = cast(int)(a * precision);
     int d = cast(int)(b * precision);
+    if (verbose) {
+        writeln("Value1: ", a, " | Value2: ", b);
+    }
     assert(c == d);
 }
 
 void assertEquals(double a, double b) {
+    if (verbose) {
+        writeln("Value1: ", a, " | Value2: ", b);
+    }
     assert(a == b);
 }
 
